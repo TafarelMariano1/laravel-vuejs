e36e54e73ff1de0e8fb82ee33f366db1cb83c53c
Jonatan Lima <jotanlima@gmail.com>
2018-09-15 10:02:53 -0300

    panels style

--- resources/assets/js/components/Panel.vue
+++ resources/assets/js/components/Panel.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="panel panel-default">
+    <div v-bind:class="defineColor">
         <div class="panel-heading">{{ title }}</div>
 
         <div class="panel-body">
@@ -10,6 +10,36 @@
 
 <script>
     export default {
-        props: ['title']
+        props: ['title', 'color'],
+        computed: {
+            defineColor: function() {
+                return `panel ${this.color} || panel-default`;
+            }
+        }
     }
 </script>
+
+<style>
+    .panel-red-clean, .panel-yellow-clean, .panel-green-clean {
+        border-color: #889BB2;
+        box-shadow: 2px 2px #dad9d9;
+    }
+
+    .panel-red-clean > .panel-heading {
+        color: #ffffff;
+        background-color: #FF9D97;
+        border-color: #889BB2;
+    }
+
+    .panel-yellow-clean > .panel-heading {
+        color: #ffffff;
+        background-color: #FFD38E;
+        border-color: #889BB2;
+    }
+
+    .panel-green-clean > .panel-heading {
+        color: #ffffff;
+        background-color: #9ECCB1;
+        border-color: #889BB2;
+    }
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,17 @@
 
                     <div class="row">
                         <div class="col-md-4">
-                            <panel title="Content 1">
+                            <panel title="Content 1" color="panel-red-clean">
                                 Test content...
                             </panel>
                         </div>
                         <div class="col-md-4">
-                            <panel title="Content 1">
+                            <panel title="Content 1" color="panel-yellow-clean">
+                                Test content...
+                            </panel>
+                        </div>
+                        <div class="col-md-4">
+                            <panel title="Content 1" color="panel-green-clean">
                                 Test content...
                             </panel>
                         </div>
